5eee8da49dd8aae6080403e6537d310e41ca1111
Demonimmortal <910539381@qq.com>
2018-08-16 23:09:59 +0800

    初始化开发环境

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css';
+import './index.scss';
+
+ReactDom.render(
+    <div>
+        <i className="fa fa-user"/>
+    </div>,
+    document.getElementById('root')
+)
